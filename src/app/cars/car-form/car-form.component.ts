bca2a6af2ff0abb771687a626ba7f63462433432
soulaichhi <chhibou.soulaimane@gmail.com>
2023-07-02 13:01:55 +0100

    modify car form

--- src/app/cars/car-form/car-form.component.ts
+++ src/app/cars/car-form/car-form.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-car-form',
   templateUrl: './car-form.component.html',
-  styleUrls: ['./car-form.component.scss']
+  styleUrls: ['./car-form.component.scss'],
 })
-export class CarFormComponent {
+export class CarFormComponent implements OnInit {
+  carForm!: FormGroup;
+  private srcResult: any;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.carForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      model: ['', Validators.required],
+      registration: ['', Validators.required],
+    });
+  }
+
+  addCar() {
+    console.log(this.carForm.value);
+  }
 }
--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -10,6 +10,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CarsListComponent, CarFormComponent],
@@ -22,6 +23,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatOptionModule,
     MatSelectModule,
     MatButtonModule,
+    ReactiveFormsModule,
   ],
 })
 export class CarsModule {}
